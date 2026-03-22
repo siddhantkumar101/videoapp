867960ba9a42ff9cf3a492f10ada12420f34f631
siddhantkumar101 <kumarsiddhant815@gmail.com>
2026-03-23 03:38:46 +0530

    created refresh access token refresh token endpoiny

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -23,4 +23,5 @@ router.route("/login").post(loginUser)
 
 //secured routes
 router.route("/logout").post(verifyJwt,logoutUser)
+router.route("/refresh-token").post(refreshAccessToken)
 export default router;
